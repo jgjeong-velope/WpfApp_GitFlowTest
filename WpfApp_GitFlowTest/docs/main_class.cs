5e3193f3f887d3488410e81b850477bac6eaf6ec
jgjeong <jgjeong@velope.co.kr>
2025-06-29 20:53:24 +0900

    main_class 생성

---
+++ WpfApp_GitFlowTest/docs/main_class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_GitFlowTest.docs
+{
+    class main_class
+    {
+        public int someProperty_byDevelop { get; set; }
+    }
+}
