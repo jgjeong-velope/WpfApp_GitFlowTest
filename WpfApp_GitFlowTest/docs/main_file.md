7c154f4ce4a86f8354e3c767714f173330aebd0d
jgjeong <jgjeong@velope.co.kr>
2025-06-29 21:17:51 +0900

    develop 내용 추가

--- WpfApp_GitFlowTest/docs/main_file.md
+++ WpfApp_GitFlowTest/docs/main_file.md
@@ -1,3 +1,7 @@
 develop 에서 생성.
 
 develop feature1 에서 추가.
+
+develop 에서 내용 추가. 
+
+develop 에서 내용 추가2
---
+++ WpfApp_GitFlowTest/docs/mainf_file2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_GitFlowTest.docs
+{
+    class mainf_file2
+    {
+        //develop에서 내용 수정
+
+        //develop에서 내용 추가2
+    }
+}
