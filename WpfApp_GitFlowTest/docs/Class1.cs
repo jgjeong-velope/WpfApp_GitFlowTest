39ba0d702d2f60657846d2f636b9cb85d5f55ebe
jgjeong <jgjeong@velope.co.kr>
2025-06-29 21:04:20 +0900

    feature1 기능 구현

---
+++ WpfApp_GitFlowTest/docs/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_GitFlowTest.docs
+{
+    class Class1
+    {
+        public int develop_feature_1{ get; set; }
+    }
+}
